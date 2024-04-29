a0065ca45885386ab044ce65e63657a7b6e35596
Charles Fyfe <charles.public.account@gmail.com>
2024-04-28 21:14:06 -0500

    condense globals

--- hardware-design/quiz4/guessing_game.s
+++ hardware-design/quiz4/guessing_game.s
@@ -1,18 +1,11 @@
     .section .rodata
-intro:
-    .ascii "I'm thinking of a number 0 to 127\n\0"
-prompt:
-    .ascii "Guess #%d: \0"
-input_format:
-    .ascii "%d\0"
-too_high_reply:
-    .ascii "Too high!\n\0"
-too_low_reply:
-    .ascii "Too low!\n\0"
-correct_reply:
-    .ascii "Correct!\n\0"
-no_more_tries:
-    .ascii "No more tries. Better luck next time!\n\0"
+intro: .ascii "I'm thinking of a number 0 to 127\n\0"
+prompt: .ascii "Guess #%d: \0"
+input_format: .ascii "%d\0"
+too_high_reply: .ascii "Too high!\n\0"
+too_low_reply: .ascii "Too low!\n\0"
+correct_reply: .ascii "Correct!\n\0"
+no_more_tries: .ascii "No more tries. Better luck next time!\n\0"
 
     .text
     .global main
@@ -23,10 +16,10 @@ main:
     sub sp, sp, #4
 
     @ use current time to generate "random" number in r4
-    sub r0, fp, #8
-    bl time
-    ldr r4, [fp, #-8]
-    and r4, r4, #0x7f
+    sub r0, fp, #8      @ no errors
+    bl time             @ in these
+    ldr r4, [fp, #-8]   @ four lines
+    and r4, r4, #0x7f   @ i promise
 
     @ init guess counter
     mov r5, #0
@@ -81,17 +74,10 @@ return:
     pop {fp, lr}
     bx lr
 
-intro_ptr:
-    .word intro
-prompt_ptr:
-    .word prompt
-input_format_ptr:
-    .word input_format
-too_high_reply_ptr:
-    .word too_high_reply
-too_low_reply_ptr:
-    .word too_low_reply
-correct_reply_ptr:
-    .word correct_reply
-no_more_tries_ptr:
-    .word no_more_tries
+intro_ptr: .word intro
+prompt_ptr: .word prompt
+input_format_ptr: .word input_format
+too_high_reply_ptr: .word too_high_reply
+too_low_reply_ptr: .word too_low_reply
+correct_reply_ptr: .word correct_reply
+no_more_tries_ptr: .word no_more_tries
