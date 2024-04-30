91c144d9e599d1595b5896e750fced23715a4aea
Charles Fyfe <charles.public.account@gmail.com>
2024-04-29 21:34:29 -0500

    add_one.s

--- hardware-design/standards/assembly-functions/add_one.s
+++ hardware-design/standards/assembly-functions/add_one.s
@@ -1,19 +1,53 @@
-@ function add_one
-	@ one parameter: x
-	@ returns x+1
+    .section .rodata
+output: .ascii "%d + 1 = %d\12\0"
 
-@ main
-	@ set up stack frame for main
-	@ local variables: num1, num2, num1_plus, num2_plus
+    .text
+    .global add_one
+add_one:
+    @ set up stack frame, no local variables
+    push {fp, lr}
+    add fp, sp, #4
+    @ input is x, return x+1
+    add r0, r0, #1
+    @ stack frame teardown
+    pop {fp, lr}
+    bx lr
 
-	@ initialize num1 to 4
+    .text
+    .global main
+main: 
+    @ stack frame setup, four local variables
+    push {fp, lr}
+    add fp, sp, #4
+    sub sp, sp, #16
+    @ fp-8 is n1, initialize to 4
+    mov r4, #4
+    str r4, [fp, #-8]
+    @ fp-12 is n2, initialize to 7
+    mov r5, #7
+    str r5, [fp, #-12]
+    @ fp-16 is n1_plus
+    mov r0, r4
+    bl add_one
+    str r0, [fp, #-16]
+    @ fp-20 is n2_plus
+    mov r0, r5
+    bl add_one
+    str r0, [fp, #-20]
+    @ Load and print n1, n1_plus
+    ldr r0, output_ptr
+    ldr r1, [fp, #-8]
+    ldr r2, [fp, #-16]
+    bl printf
+    @ Load and print n2, n2_plus
+    ldr r0, output_ptr
+    ldr r1, [fp, #-12]
+    ldr r2, [fp, #-20]
+    bl printf
+    @ stack frame teardown, return 0
+    mov r0, #0
+    sub sp, fp, #4
+    pop {fp, pc}
 
-	@ initialize num2 to 7
+output_ptr: .word output
 
-	@ call add_one on num1, and store the result to num1_plus
-
-	@ call add_one on num2, and store the result to num2_plus
-
-	@ return 0
-
-	@ tear down stack frame for mai
