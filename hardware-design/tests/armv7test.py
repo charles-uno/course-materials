babbe00a4af5d14240816465efb70b9543b0b220
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2025-01-29 20:47:22 -0600

    good enough for a first test

--- hardware-design/tests/armv7test.py
+++ hardware-design/tests/armv7test.py
@@ -12,17 +12,32 @@ from dataclasses import dataclass
 class TestArgs:
     src: str
     input: Optional[str]
-    output: Optional[str]
+    output: str
 
 
 def main() -> int:
     args = parse_args()
     try:
         build_run_and_check(args)
+        print_with_color("ok", color="green")
         return 0
     except TestFailure as exc:
-        print(exc)
+        print_with_color(str(exc), color="red")
         return 1
+
+
+def print_with_color(*args, color=None):
+    text = " ".join(args)
+    if color == "green":
+        print("\033[92m" + text + "\033[0m")
+    elif color == "red":
+        print("\033[31m" + text + "\033[0m")
+    elif color == "magenta":
+        print("\033[35m" + text + "\033[0m")
+    elif color == "yellow":
+        print("\033[93m" + text + "\033[0m")
+    else:
+        print(text + "\033[0m")
 
 
 def build_run_and_check(args: TestArgs):
@@ -33,28 +48,40 @@ def build_run_and_check(args: TestArgs):
     sp.getoutput(f"gcc {args.src} -o a.out")
     if not os.path.isfile("a.out"):
         raise TestFailure("build failed")
-    # Note: no input is potentially different from empty string input
-    if input is None:
-        stdout = sp.getoutput("./a.out")
+    proc = sp.Popen(["./a.out"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE)
+    # Watch out - subprocess uses bytes, not unicode strings
+    if args.input is None:
+        proc_reply = proc.communicate()
     else:
-        proc = sp.Popen(
-            ["./a.out"], stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE, text=True
+        proc_reply = proc.communicate(input=args.input.encode("ascii"))
+    stdout = proc_reply[0].decode("utf-8")
+    # Don't worry about trailing whitespace or newlines
+    if stdout.rstrip() != args.output.rstrip():
+        raise TestFailure(
+            f"expected output '{args.output.rstrip()}' but got '{stdout.rstrip()}'"
         )
-        proc_reply = proc.communicate(input=args.input)
-        stdout = proc_reply[0]
-        return_code = proc_reply[2]
-    if args.output and stdout != args.output:
-        raise TestFailure(f"expected output '{args.output}' but got '{stdout}'")
-    if return_code != 0:
-        raise TestFailure(f"expected return code 0 but got '{return_code}'")
+    if proc.return_code != 0:
+        stderr = proc_reply[1].decode("utf-8").strip()
+        if "\n" in stderr:
+            stderr = stderr.splitlines()[0] + "..."
+        raise TestFailure(
+            f"got unexpected returncode {proc.return_code}, stderr: '{stderr}'"
+        )
 
 
 def parse_args() -> TestArgs:
     parser = argparse.ArgumentParser(description="test tool for arm v7 via gcc")
     parser.add_argument("src", help="name of the assembly file to build and test")
+    # Note: no input is potentially different from empty string input
     parser.add_argument("-i", "--input", help="input string to provide to the program")
-    parser.add_argument("-o", "--output", help="expected output from the build program")
-    return vars(parser.parse_args())
+    parser.add_argument(
+        "-o", "--output", help="expected output from the build program", default=""
+    )
+    args = parser.parse_args()
+    # Use a daraclass for typing
+    return TestArgs(
+        src=args.src, input=vars(args).get("input"), output=vars(args).get("output")
+    )
 
 
 class TestFailure(Exception):
---
+++ hardware-design/tests/example-test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# exit on error
+set -e
+
+./armv7test.py char_flipper.s -i 'g' -o 'Enter a character: Flipped case: G'
