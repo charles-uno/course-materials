d88297a31cd387ec764299f81d29e733f7401951
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2025-12-28 15:54:08 -0600

    printf examples

---
+++ hardware-design/code/globals/hello.c
@@ -0,0 +1,13 @@
+		#include <stdio.h>
+
+		int main() {
+
+		const char* name = "charles";
+		const int year = 2026;
+
+		printf("hello my name is %s\n", name);
+
+		printf("the current year is %d\n", year);
+
+			return 0;
+		}
