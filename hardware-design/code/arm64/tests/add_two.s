51d4fde19794b73ba1035db764236b5bbb663bcd
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-01-06 11:25:45 -0600

    local variable fix

--- hardware-design/code/arm64/tests/add_two.s
+++ hardware-design/code/arm64/tests/add_two.s
@@ -10,7 +10,7 @@
 main:
     // stack frame setup, two local variables
     sub sp, sp, 32
-    str fp, [sp, -8]
+    str fp, [sp, 8]
     str lr, [sp]
     add fp, sp, 24
 
@@ -27,13 +27,13 @@ main:
     bl  scanf
 
     ldr x0, [fp]
-    ldr x1, [fp, 8]
+    ldr x1, [fp, -8]
     add x1, x0, x1
     ldr x0, =reply
     bl printf
 
     ldr lr, [sp]
-    ldr fp, [sp, -8]
+    ldr fp, [sp, 8]
     add sp, sp, 32
     ret
 
