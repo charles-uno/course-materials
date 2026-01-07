e0664e41a709af5aafe56d5bea77c15db8e6088c
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-01-06 22:28:56 -0600

    char flipper example

--- hardware-design/code/arm64/conditionals/char_flipper.s
+++ hardware-design/code/arm64/conditionals/char_flipper.s
@@ -9,17 +9,18 @@ flip_char_case:
     sub sp, sp, 16
     str fp, [sp]
     str lr, [sp, 8]
-    // input r0 is an ascii char
+    // we only care about the last 8 bits. ignore the rest
+    and x0, x0, 0xff
     // for uppercase letter (65-90) return the corresponding lowercase ascii value
     // for lowercase letter (97-122) return the corresponding uppercase ascii value
     // anything else return the input unchanged
-    cmp x0, 65
+    cmp x0, 'A'
     blt return
-    cmp x0, 90
+    cmp x0, 'Z'
     ble upper_to_lower
-    cmp x0, 97
+    cmp x0, 'a'
     blt return
-    cmp x0, 122
+    cmp x0, 'z'
     ble lower_to_upper
     b return
 upper_to_lower:
@@ -46,12 +47,13 @@ main:
     bl printf
     ldr x0, =char_format
     add x1, sp, 16
-    bl  scanf
+    bl scanf
 
     ldr x0, [sp, 16]
     bl flip_char_case
+
     mov x1, x0
-    ldr x0, reply_ptr
+    ldr x0, =reply
     bl printf
 
     // return 0, stack frame teardown
