c23bf74074ce49de5aa8a3f3fd630c75b92080e7
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-01-07 14:55:12 -0600

    example test

--- hardware-design/code/arm64/tests/add_two_test.sh
+++ hardware-design/code/arm64/tests/add_two_test.sh
@@ -1,58 +1,50 @@
 #!/usr/bin/bash
 
+# exit as soon as we hit an error
+# set -e
+
+function run_and_check {
+    local TEST_CASE_NAME="$1"
+    local INPUT="$2"
+    local EXPECTED_OUTPUT="$3"
+    ACTUAL_OUTPUT=$(echo "$INPUT" | ./a.out) ||:
+    if [[ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]]; then
+        echo "$TEST_CASE_NAME - expected \"$EXPECTED_OUTPUT\" but got \"$ACTUAL_OUTPUT\""
+    else
+        echo "$TEST_CASE_NAME - ok"
+    fi
+}
+
+
 # Case 0: compile the code
-
-TEST_CASE_NAME="compile"
-
-rm a.out 
+CASE_NAME="compile"
+rm a.out ||:
 gcc add_two.s 
 if [[ "$?" != "0" || -f a.out ]]; then 
-    echo "$TEST_CASE_NAME - ok"
+    echo "$CASE_NAME - ok"
 else
-    echo "$TEST_CASE_NAME - build failed"
+    echo "$CASE_NAME - build failed"
 fi
 
 # Case 1: small positive ints
-
-TEST_CASE_NAME="positive"
-
+CASE_NAME="positive"
 INPUT="3
 4"
 EXPECTED_OUTPUT="Enter a number: Enter a number: Sum: 7"
-ACTUAL_OUTPUT=$(echo "$INPUT" | ./a.out)
-
-if [[ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]]; then
-    echo "$TEST_CASE_NAME - expected \"$EXPECTED_OUTPUT\" but got \"$ACTUAL_OUTPUT\""
-else
-    echo "$TEST_CASE_NAME - ok"
-fi
+run_and_check "$CASE_NAME" "$INPUT" "$EXPECTED_OUTPUT"
 
 # Case 2: sum to zero
-
-TEST_CASE_NAME="opposite"
-
+CASE_NAME="opposite"
 INPUT="-3
 3"
 EXPECTED_OUTPUT="Enter a number: Enter a number: Sum: 0"
-ACTUAL_OUTPUT=$(echo "$INPUT" | ./a.out)
-
-if [[ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]]; then
-    echo "$TEST_CASE_NAME - expected \"$EXPECTED_OUTPUT\" but got \"$ACTUAL_OUTPUT\""
-else
-    echo "$TEST_CASE_NAME - ok"
-fi
+run_and_check "$CASE_NAME" "$INPUT" "$EXPECTED_OUTPUT"
 
 # Case 3: negatives
-
-TEST_CASE_NAME="negative"
-
+CASE_NAME="negative"
 INPUT="-3
 -106"
 EXPECTED_OUTPUT="Enter a number: Enter a number: Sum: -109"
-ACTUAL_OUTPUT=$(echo "$INPUT" | ./a.out)
+run_and_check "$CASE_NAME" "$INPUT" "$EXPECTED_OUTPUT"
 
-if [[ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]]; then
-    echo "$TEST_CASE_NAME - expected \"$EXPECTED_OUTPUT\" but got \"$ACTUAL_OUTPUT\""
-else
-    echo "$TEST_CASE_NAME - ok"
-fi
+
