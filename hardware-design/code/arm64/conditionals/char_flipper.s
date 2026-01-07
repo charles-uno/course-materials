e434eda35bd0dfd8083d0997fc7cbaf02241c22d
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-01-06 22:11:16 -0600

    multiply 3 example

---
+++ hardware-design/code/arm64/conditionals/char_flipper.s
@@ -0,0 +1,62 @@
+.section .rodata
+    input_prompt: .ascii "Enter a character: \0"
+    char_format: .ascii "%c\0"
+    reply: .ascii "Flipped case: %c\12\0"
+
+.text
+flip_char_case:
+    // stack frame setup, no local variables
+    sub sp, sp, 16
+    str fp, [sp]
+    str lr, [sp, 8]
+    // input r0 is an ascii char
+    // for uppercase letter (65-90) return the corresponding lowercase ascii value
+    // for lowercase letter (97-122) return the corresponding uppercase ascii value
+    // anything else return the input unchanged
+    cmp x0, 65
+    blt return
+    cmp x0, 90
+    ble upper_to_lower
+    cmp x0, 97
+    blt return
+    cmp x0, 122
+    ble lower_to_upper
+    b return
+upper_to_lower:
+    add x0, x0, 32
+    b return
+lower_to_upper:
+    sub x0, x0, 32
+    b return
+return:
+    ldr lr, [sp, 8]
+    ldr fp, [sp]
+    add sp, sp, 16
+    ret
+
+.text
+.global main
+main: 
+    sub sp, sp, 32
+    str fp, [sp]
+    str lr, [sp, 8]
+    add fp, sp, 24
+
+    ldr x0, =input_prompt
+    bl printf
+    ldr x0, =char_format
+    add x1, sp, 16
+    bl  scanf
+
+    ldr x0, [sp, 16]
+    bl flip_char_case
+    mov x1, x0
+    ldr x0, reply_ptr
+    bl printf
+
+    // return 0, stack frame teardown
+    mov x0, 0
+    ldr lr, [sp, 8]
+    ldr fp, [sp]
+    add sp, sp, 32
+    ret
