fed0e0a5b6c6f1352bcc313c0493cbdd39aaccd1
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-02-09 14:14:58 -0600

    c to assembly exercise

---
+++ hardware-design/code/c/example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    
+    int a = 4;
+    int b = 7;
+    
+    const int c = 12;
+    
+    int main() {
+        printf("a is %d.\n", a);
+        printf("b is %d.\n", b);
+        printf("c is %d.\n", c);
+        return 0;
+    }
