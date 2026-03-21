89e5685cceece7be6c0f4b3cd15135ad8c6eee67
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-03-20 22:33:39 -0500

    organize code

---
+++ hardware-design/chapters/03-instructions-on-hardware/code/float-assoc.c
@@ -0,0 +1,13 @@
+
+
+
+// floating point operations generally cannot be reordered due to slight changes in the results
+
+// sum = (a + b) + (c + d);
+
+// vs
+
+// sum = ((a + b) + c) + d;
+
+// use -O0 and -O3 and see if the compiler tries to fix it?
+
