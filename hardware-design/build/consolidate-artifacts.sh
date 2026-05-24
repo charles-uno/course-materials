8334b11233b387fb86ea09538ab9a8b16e8b88e9
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2026-05-23 21:50:47 -0500

    tidy up artifact names

--- hardware-design/build/consolidate-artifacts.sh
+++ hardware-design/build/consolidate-artifacts.sh
@@ -2,20 +2,22 @@
 
 set -e
 
+if [[ -d artifacts ]]; then rm -r artifacts; fi
 mkdir -p artifacts
-rm -r artifacts/*
 
 for X in $(ls */*.pdf); do
     FILENAME=$(basename $X)
     printf "moving \033[35m$X\033[0m -> \033[35martifacts/$FILENAME\033[0m\n"
     cp "$X" artifacts/
-done    
+done
 
 for X in $(ls chapters/*/*.pdf); do
-    FILENAME_WITHOUT_EXT=$(basename "$X" | cut -d '.' -f 1)
+    FILENAME=$(basename "$X")
     CHAPTERNAME=$(dirname $X | xargs basename)
-    printf "moving \033[35m$X\033[0m -> \033[35martifacts/$FILENAME_WITHOUT_EXT-$CHAPTERNAME.pdf\033[0m\n"
-    cp "$X" "artifacts/$FILENAME_WITHOUT_EXT-$CHAPTERNAME.pdf"
+#    INITIALS=$(echo "$CHAPTERNAME" | sed -E 's/([a-zA-Z])[a-zA-Z]*-?/\1/g')
+    NEW_FILENAME="$CHAPTERNAME-$FILENAME"
+    printf "moving \033[35m$X\033[0m -> \033[35martifacts/$NEW_FILENAME\033[0m\n"
+    cp "$X" "artifacts/$NEW_FILENAME"
 done
 
 
